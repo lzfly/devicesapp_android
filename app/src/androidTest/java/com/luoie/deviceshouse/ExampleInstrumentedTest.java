164248e69d0d96c2ed41dffbba8dd8414a8e2716
luoie <luoiefly@163.com>
2017-12-17 14:04:13 +0800

    change folder name

--- app/src/androidTest/java/com/luoie/deviceshouse/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/luoie/deviceshouse/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.lapsen.robot", appContext.getPackageName());
+        assertEquals("com.luoie.deviceshouse", appContext.getPackageName());
     }
 }
